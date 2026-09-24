095f97c8ac73dbf3841d6075d4927b74b20e7d64
n8theAdequate <nathan@nathandavies.tv>
2026-09-24 02:26:41 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'brick-shiftout-v1';
+const ASSETS = [
+  './',
+  './Brick%20ShiftOut.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
